a3beeb0da4b4674fc683b5f5551f1f8e398f7c0d
Marcell Borges <marcell.borges@gmail.com>
2022-08-22 10:03:02 -0300

    front almost done

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('cars', views.cars, name='cars'),
+    path('search', views.search, name='search'),
+    path('services', views.services, name='services'),
+    path('car_details', views.car_details, name='car_details'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,31 @@ from django.shortcuts import HttpResponse, render
 def index(request):
     return render(request, 'pages/index.html')
 
-
 def login(request):
     return render(request, 'pages/login.html')
 
-
 def signup(request):
     return render(request, 'pages/signup.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def search(request):
+    return render(request, 'pages/search.html')
+    
+def about(request):
+    return render(request, 'pages/about.html')
+
+def car_details(request):
+    return render(request, 'pages/car_details.html')
+
+
